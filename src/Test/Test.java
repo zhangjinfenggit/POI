defc5b7c84d7f393f2fd4b557a2e5e5cfa88b653
zhangjinfenggit <1412619418@qq.com>
2017-05-30 14:41:06 +0800

    git 测试

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -41,7 +41,7 @@ public class Test {
 		FileOutputStream fileOutputStream = new FileOutputStream("C:/111.xls");
 
 		workbook.write(fileOutputStream);
-		cell.setCellStyle(zhangsan);
+		workbook.write(fo);
 
 	}
 }
