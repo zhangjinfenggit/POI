93b053b9624f0179e7de808ca5c4086ebe4a0422
zhangjinfenggit <1412619418@qq.com>
2017-05-30 14:40:00 +0800

    git 测试
    一会别忘了删除workbook.write(fo);

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -41,7 +41,7 @@ public class Test {
 		FileOutputStream fileOutputStream = new FileOutputStream("C:/111.xls");
 
 		workbook.write(fileOutputStream);
-		cell.setCellStyle(zhangsan);
-		//你是谁
+		workbook.write(fo);
+		
 	}
 }
